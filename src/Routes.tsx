263273c4ed71a2cb639616870ec217eec6a2201c
danielbped <danielbped@gmail.com>
2024-03-24 19:21:17 -0300

    chore: adiciona rota product details

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
-import Products from './pages/ProductDetail';
+import Products from './pages/Products';
 import Register from './pages/Register';
+import ProductsDetails from './pages/ProductDetail';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/products" element={<Products />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/product/:id" element={<ProductsDetails />} />
     </Routes>
   </BrowserRouter>
 );
