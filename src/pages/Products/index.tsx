f1ffb98db3f66037056779b67aac4dba0af07bc5
danielbped <danielbped@gmail.com>
2024-03-24 19:24:27 -0300

    feat: pagina inicial de produtos

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,9 +1,21 @@
-const ProductDetails = () => {
+import { useNavigate } from "react-router-dom";
+import Loading from "../../components/Loading";
+import ProductList from "../../components/ProductList";
+import useProductData from "../../hooks/useProductData";
+
+const Products = () => {
+  const { data, isLoading, error, isError } = useProductData();
+
+  const navigate = useNavigate();
+  const handleNavigate = (route: string) => navigate(`/${route}`)
+
   return (
-    <h1>
-      Detalhes do Produto
-    </h1>
+    <div>
+      { isLoading && <Loading /> }
+      { isError && <p>{ error.response.data.message }</p> }
+      { !isLoading && data && <ProductList data={ data } handleNavigate={ handleNavigate } />}
+    </div>
   );
 };
 
-export default ProductDetails;
+export default Products;
