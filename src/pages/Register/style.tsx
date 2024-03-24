fd609343c7361129e14dc3f9b526383910c3dfa8
danielbped <danielbped@gmail.com>
2024-03-24 17:57:39 -0300

    feat: estilização basica de cadastro

---
+++ src/pages/Register/style.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledRegister = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+export {
+  StyledRegister
+};
