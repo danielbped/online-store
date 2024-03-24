56602ea0635540f9b17c09c9ba1ba4fa73f35650
danielbped <danielbped@gmail.com>
2024-03-24 16:33:10 -0300

    feat: adiciona funcionalidades para input padrão

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,25 @@
-const Form = () => {
+import { ChangeEventHandler } from "react";
+
+interface InputProps {
+  type: string,
+  name: string,
+  placeholder: string,
+  title: string,
+  onChange: ChangeEventHandler<HTMLInputElement>
+}
+
+const Input = (props: InputProps) => {
   return (
-    <h1>
-      Input
-    </h1>
+    <div>
+      <label>{ props.title }</label>
+      <input
+        type={ props.type }
+        name={ props.name }
+        placeholder={ props.placeholder }
+        onChange={ props.onChange }
+      />
+    </div>
   );
 };
 
-export default Form;
+export default Input;
