5016da840ec1aa8704de42acc75c5e3fedc9b679
danielbped <danielbped@gmail.com>
2024-03-24 17:57:29 -0300

    feat: implementa pagina de cadastro com suas devidas funções

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,8 +1,57 @@
+import { ChangeEventHandler, useEffect, useState } from "react";
+import RegisterForm from "../../components/RegisterForm";
+import { StyledRegister } from "./style";
+import { RegisterData } from "../../interfaces/register-data.interface";
+import useRegisterMutate from "../../hooks/useRegisterData";
+import { useNavigate } from "react-router-dom";
+
 const Register = () => {
+  const [registerData, setRegisterData] = useState<RegisterData>({
+    firstName: undefined,
+    lastName: undefined,
+    email: undefined,
+    password: undefined,
+    confirmPassword: undefined
+  });
+
+  const { mutate, isSuccess, isError, error } = useRegisterMutate();
+
+  const handleRegister: ChangeEventHandler<HTMLInputElement> = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setRegisterData({ ...registerData, [name]: value });
+  };
+
+  const disableSubmit = () => {
+    const { password, confirmPassword } = registerData;
+
+    return Object.values(registerData).some(field => !field)
+      || password !== confirmPassword;
+  }
+
+  const navigate = useNavigate();
+  const handleNavigate = (route: string) => navigate(`/${route}`)
+
+  const submit = () => {
+    !disableSubmit() && mutate(registerData)
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      handleNavigate('login')
+    }
+  }, [isSuccess]);
+
   return (
-    <h1>
-      Registras
-    </h1>
+    <StyledRegister>
+      <RegisterForm
+        handleRegister={ handleRegister }
+        submit={ submit }
+        isError={ isError }
+        error={ error?.response.data.message }
+        handleNavigate={ handleNavigate }
+        disabledButton={ disableSubmit() }
+      />
+    </StyledRegister>
   );
 };
 
