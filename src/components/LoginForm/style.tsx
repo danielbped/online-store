1f0f7eda58ab31b2eeab50ed46353702dc653f26
danielbped <danielbped@gmail.com>
2024-03-24 16:33:41 -0300

    feat: adiciona estilização básica para pagina de login

---
+++ src/components/LoginForm/style.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const StyledButtonSection = styled.div`
+  display: flex;
+`;
+
+const StyledInputSection = styled.div`
+
+`;
+
+const StyledLoginForm = styled.div`
+
+`;
+
+export {
+  StyledButtonSection,
+  StyledInputSection,
+  StyledLoginForm,
+}
