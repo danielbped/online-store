a5f6e9cd0a547a1b47aee5963fcd293ce7d6f211
danielbped <danielbped@gmail.com>
2024-03-24 17:55:49 -0300

    feat: estilizacao basica de form de cadastro

---
+++ src/components/RegisterForm/style.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const StyledButtonSection = styled.div`
+  display: flex;
+`;
+
+const StyledInputSection = styled.div`
+
+`;
+
+const StyledRegisterForm = styled.div`
+
+`;
+
+export {
+  StyledButtonSection,
+  StyledInputSection,
+  StyledRegisterForm,
+}
