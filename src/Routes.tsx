8c91bffa81754feb46d1e5986643c29e4a192d40
danielbped <danielbped@gmail.com>
2024-03-25 00:29:29 -0300

    feat: reestrutura routes para o header aparecer para algumas rotas

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,6 +4,7 @@ import Products from './pages/Products';
 import Register from './pages/Register';
 import ProductsDetails from './pages/ProductDetail';
 import Favorites from './pages/Favorites';
+import Header from './components/Header';
 
 const App = () => (
   <BrowserRouter>
@@ -11,11 +12,32 @@ const App = () => (
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/product/:id" element={<ProductsDetails />} />
-      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/products" element={<HeaderAndProducts />} />
+      <Route path="/product/:id" element={<HeaderAndProductDetails />} />
+      <Route path="/favorites" element={<HeaderAndFavorites />} />
     </Routes>
   </BrowserRouter>
+);
+
+const HeaderAndProducts = () => (
+  <>
+    <Header />
+    <Products />
+  </>
+);
+
+const HeaderAndProductDetails = () => (
+  <>
+    <Header />
+    <ProductsDetails />
+  </>
+);
+
+const HeaderAndFavorites = () => (
+  <>
+    <Header />
+    <Favorites />
+  </>
 );
 
 export default App;
