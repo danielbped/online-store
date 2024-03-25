77a3c7b8a715315c864149b9fcf82d9d1d670bec
danielbped <danielbped@gmail.com>
2024-03-25 19:04:37 -0300

    fix: ajuste na pagina de favoritos

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -5,18 +5,31 @@ import ErrorMessage from "../../components/ErrorMessage";
 import { useSelector } from "react-redux";
 import { useLocalStorage } from "@uidotdev/usehooks";
 import { LoginResponse } from "../../interfaces/login-data.interface";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Favorites = () => {
   const [token, setToken] = useLocalStorage<LoginResponse | null>("token", null);
 
   const user = useSelector((rootReducer: any) => rootReducer.userReducer);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user.id === '') {
+      setToken(null);
+      navigate('/login');
+    }
+  }, [user.id])
+
   const { data, isLoading, error, isError } = useFavoritesData(user.id, token);
+
+  const products = data ? data.map((product) => ({ ...product, favorite: true })) : [];
 
   return (
     <div>
       { isLoading && <Loading /> }
       { isError && <ErrorMessage message={ error.response.data.message } /> }
-      { !isLoading && data && <ProductList data={ data } />}
+      { !isLoading && data && <ProductList data={ products } />}
     </div>
   );
 };
